365dcf1fec81ff4fe4a10697f7a9946b7b113df0
Laserology <167719528+Laserology@users.noreply.github.com>
2025-07-03 13:34:45 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,10 +10,14 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux zsh btop fastfetch ripgrep fd-find
+dnf5 install -y tmux fastfetch
 
 # remove firefox in favour of using the flatpak
 dnf5 remove -y firefox
+
+dnf5 install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf5 install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf5 install akmod-nvidia xorg-x11-drv-nvidia-cuda xorg-x11-drv-nvidia-cuda-libs
 
 # Use a COPR Example:
 #
